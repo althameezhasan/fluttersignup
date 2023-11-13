be257635950b15450e85f653b13f96f509c57740
athameezhasan <athameez@blackitechs.com>
2023-11-13 20:07:23 +0530

    the changes

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/screens/signin_screen.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    body: Center(
+      child: ElevatedButton(
+        child: Text("Log out"),
+        onPressed: (){
+          Navigator.push(context,
+           MaterialPageRoute(builder: (context) => SignInScreen(),));
+        },),
+    ),
+    );
+  }
+}
